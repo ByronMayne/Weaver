c597d29bc8137e76f315c499c993148358a885f5
Byron Mayne <ByronMayne@gmail.com>
2017-09-03 19:22:46 -0400

    Added two template attributes for future use.

---
+++ Assets/Weaver/Editor/Addins/MethodTimer/MethodTimerAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weaver
+{
+    /// <summary>
+    /// Based off of https://github.com/Fody/PropertyChanged
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class MethodTimerAttribute : Attribute
+    {
+    }
+}
