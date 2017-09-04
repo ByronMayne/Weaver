9e5b5587b6f4d0de115d372b1d5f59c9fb582b09
Byron Mayne <ByronMayne@gmail.com>
2017-09-04 13:35:13 -0400

    Added new WeaverAssemblyResolver to help find Unity Assemblies

---
+++ Assets/Weaver/Editor/Resolver/WeaverAssemblyResolver.cs
@@ -0,0 +1,94 @@
+﻿using Mono.Cecil;
+using System;
+using UnityEditorInternal;
+
+namespace Weaver
+{
+    /// <summary>
+    /// Used to resolve any references to the Unity Editor or UnityEngine assemblies.
+    /// </summary>
+    public class WeaverAssemblyResolver : DefaultAssemblyResolver
+    {
+        public override AssemblyDefinition Resolve(string fullName)
+        {
+            if(IsUnityEngineAssembly(fullName))
+            {
+                return GetUnityEngineAssemblyDef();
+            }
+
+            if(IsUnityEditorAssembly(fullName))
+            {
+                return GetUnityEditorAssemblyDef(); 
+            }
+
+            return base.Resolve(fullName);
+        }
+
+        public override AssemblyDefinition Resolve(AssemblyNameReference name)
+        {
+            if (IsUnityEngineAssembly(name.FullName))
+            {
+                return GetUnityEngineAssemblyDef();
+            }
+
+            if (IsUnityEditorAssembly(name.FullName))
+            {
+                return GetUnityEditorAssemblyDef();
+            }
+
+            return base.Resolve(name);
+        }
+
+        public override AssemblyDefinition Resolve(AssemblyNameReference name, ReaderParameters parameters)
+        {
+            if (IsUnityEngineAssembly(name.FullName))
+            {
+                return GetUnityEngineAssemblyDef();
+            }
+
+            if (IsUnityEditorAssembly(name.FullName))
+            {
+                return GetUnityEditorAssemblyDef();
+            }
+
+            return base.Resolve(name, parameters);
+        }
+
+        public override AssemblyDefinition Resolve(string fullName, ReaderParameters parameters)
+        {
+            if (IsUnityEngineAssembly(fullName))
+            {
+                return GetUnityEngineAssemblyDef();
+            }
+
+            if (IsUnityEditorAssembly(fullName))
+            {
+                return GetUnityEditorAssemblyDef();
+            }
+
+            return base.Resolve(fullName, parameters);
+        }
+
+        private static bool IsUnityEditorAssembly(string name)
+        {
+            return name.Equals("UnityEditor, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null", StringComparison.Ordinal);
+        }
+
+        private static bool IsUnityEngineAssembly(string name)
+        {
+            return name.Equals("UnityEngine, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null", StringComparison.Ordinal);
+        }
+
+        private static AssemblyDefinition GetUnityEngineAssemblyDef()
+        {
+            string engineAssemblyPath = InternalEditorUtility.GetEngineAssemblyPath();
+            return AssemblyDefinition.ReadAssembly(engineAssemblyPath);
+        }
+
+        private static AssemblyDefinition GetUnityEditorAssemblyDef()
+        {
+            string editorAssemblyPath = InternalEditorUtility.GetEditorAssemblyPath();
+            return AssemblyDefinition.ReadAssembly(editorAssemblyPath);
+        }
+    }
+}
--- Assets/Weaver/Editor/Settings/WeaverSettings.cs
+++ Assets/Weaver/Editor/Settings/WeaverSettings.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 using Mono.Cecil;
+using Mono.Collections.Generic;
 
 namespace Weaver
 {
@@ -18,6 +19,14 @@ namespace Weaver
 
         // Watchers
         private List<FileSystemWatcher> m_AssemblyWatches;
+
+        // Resolver
+        private WeaverAssemblyResolver m_Resolver; 
+
+        public WeaverAssemblyResolver resolver
+        {
+            get { return m_Resolver; }
+        }
 
         [InitializeOnLoadMethod]
         private static void Initialize()
@@ -47,23 +56,83 @@ namespace Weaver
         /// </summary>
         private void OnAssemblyChanged(string filePath)
         {
+            // Create new resolver
+            m_Resolver = new WeaverAssemblyResolver();
+
+            // Clean up any missing addins so we don't have to null check all the time
+            for (int i = m_Addins.Count - 1; i >= 0; i--)
+            {
+                if (m_Addins[i] == null)
+                {
+                    m_Addins.RemoveAt(i);
+                }
+                else
+                {
+                    m_Addins[i].Initialize(this);
+                }
+            }
             // Save our write times to disk.
             EditorUtility.SetDirty(this);
+            // Create reader settings
+            ReaderParameters readerParameters = new ReaderParameters();
+            readerParameters.AssemblyResolver = m_Resolver;
             // Load our definition
-            ModuleDefinition moduleDefinition = ModuleDefinition.ReadModule(filePath);
-            // Make sure it's not null
-            if (moduleDefinition != null)
+            ModuleDefinition moduleDefinition = ModuleDefinition.ReadModule(filePath, readerParameters);
+            // Call init on each of our addins
+            for (int i = 0; i < m_Addins.Count; i++)
             {
-                // Call init on each of our addins
+                if (m_Addins[i].EffectsDefintion(DefinitionType.Module))
+                {
+                    m_Addins[i].VisitModule(moduleDefinition);
+                }
+            }
+            // TYPES
+            foreach(TypeDefinition typeDefinition in moduleDefinition.Types)
+            {
                 for (int i = 0; i < m_Addins.Count; i++)
                 {
-                    if (m_Addins[i] != null)
+                    if (m_Addins[i].EffectsDefintion(DefinitionType.Type))
                     {
-                        m_Addins[i].Initialize(moduleDefinition);
+                        m_Addins[i].VisitType(typeDefinition);
+                    }
+                }
+
+                // METHODS
+                foreach(MethodDefinition methodDefinition in typeDefinition.Methods)
+                {
+                    for (int i = 0; i < m_Addins.Count; i++)
+                    {
+                        if (m_Addins[i].EffectsDefintion(DefinitionType.Method))
+                        {
+                            m_Addins[i].VisitMethod(methodDefinition);
+                        }
+                    }
+                }
+
+                // PROPERTIES
+                foreach (PropertyDefinition propertyDefinition in typeDefinition.Properties)
+                {
+                    for (int i = 0; i < m_Addins.Count; i++)
+                    {
+                        if (m_Addins[i].EffectsDefintion(DefinitionType.Property))
+                        {
+                            m_Addins[i].VisitProperty(propertyDefinition);
+                        }
+                    }
+                }
+
+                // FIELDS
+                foreach (FieldDefinition fieldDefinition in typeDefinition.Fields)
+                {
+                    for (int i = 0; i < m_Addins.Count; i++)
+                    {
+                        if (m_Addins[i].EffectsDefintion(DefinitionType.Field))
+                        {
+                            m_Addins[i].VisitField(fieldDefinition);
+                        }
                     }
                 }
             }
-
             // Write the values to disk
             moduleDefinition.Write(filePath);
         }
