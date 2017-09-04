ad0947bffd8fe44648cbe91e26f32ba6e37e552f
Byron Mayne <ByronMayne@gmail.com>
2017-09-03 23:48:10 -0400

    Created a simple timer method that does pretty much nothing.

--- Assets/Weaver/Addins/MethodTimer/Editor/MethodTimerAddin.cs
+++ Assets/Weaver/Addins/MethodTimer/Editor/MethodTimerAddin.cs
@@ -2,6 +2,7 @@
 using Mono.Cecil;
 using UnityEngine;
 using Mono.Cecil.Cil;
+using System.Linq;
 using System.Diagnostics;
 
 namespace Weaver
@@ -29,9 +30,37 @@ namespace Weaver
             TypeSystem typeSystem = moduleDefinition.TypeSystem;
 
             // Create type
-            TypeAttributes typeAttributes = TypeAttributes.BeforeFieldInit | TypeAttributes.AnsiClass | TypeAttributes.AutoClass;
-            TypeDefinition stopwatchType = new TypeDefinition("Weaver", "MethodTimer", typeAttributes, typeSystem.Object);
-            moduleDefinition.Types.Add(stopwatchType);
+            TypeAttributes typeAttributes = TypeAttributes.BeforeFieldInit;
+            TypeDefinition methodTimerTypeDefinition = new TypeDefinition("Weaver.Generated", "MethodTimer", typeAttributes, typeSystem.Object);
+            moduleDefinition.Types.Add(methodTimerTypeDefinition);
+
+            // Import Date Time
+            TypeReference stopwatchTypeReference = moduleDefinition.Import(typeof(Stopwatch));
+
+            // Create a StopWatch field
+            FieldDefinition stopwatchField = new FieldDefinition("m_StopWatch", FieldAttributes.Private | FieldAttributes.Static, stopwatchTypeReference);
+            methodTimerTypeDefinition.Fields.Add(stopwatchField);
+
+            // StartTimer Method
+            MethodDefinition StartMethod = new MethodDefinition("Start", MethodAttributes.Public | MethodAttributes.Static, typeSystem.Void);
+            methodTimerTypeDefinition.Methods.Add(StartMethod);
+
+            // StopTimer Method
+            MethodDefinition StopMethod = new MethodDefinition("Stop", MethodAttributes.Public | MethodAttributes.Static, typeSystem.Void);
+            methodTimerTypeDefinition.Methods.Add(StopMethod);
+
+            TypeDefinition stopWatchTypeDefinition = stopwatchTypeReference.Resolve();
+            MethodDefinition stopWatchStartMethod = stopWatchTypeDefinition.Methods.FirstOrDefault(x => x.Name == "Start");
+            MethodBody startMethodBody = StartMethod.Body;
+
+            var startMetho =  moduleDefinition.Import(stopWatchStartMethod);
+
+            Instruction One = Instruction.Create(OpCodes.Ldsfld, stopwatchField);
+            Instruction Two = Instruction.Create(OpCodes.Callvirt, startMetho);
+            Instruction Three = Instruction.Create(OpCodes.Ret);
+            startMethodBody.Instructions.Add(One);
+            startMethodBody.Instructions.Add(Two);
+            startMethodBody.Instructions.Add(Three);
         }
 
         public override void OnMatch(Type attributeType, MemberReference effectedMemeber)
