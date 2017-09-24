fb9f5118be8dcfb0db9fc69c63a2de479d4d3398
ByronMayne <ByronMayne@gmail.com>
2017-09-24 11:06:19 -0400

    Added a new extension to get methods based on it's parameter types

--- Assets/Weaver/Editor/Type Extensions/TypeDefinitionExtensions.cs
+++ Assets/Weaver/Editor/Type Extensions/TypeDefinitionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Mono.Cecil;
+using System;
 
 namespace Weaver
 {
@@ -16,6 +17,36 @@ namespace Weaver
                 if (string.Compare(methodDef.Name, name) == 0)
                 {
                     return methodDef;
+                }
+            }
+            return null;
+        }
+
+        public static MethodDefinition GetMethod(this TypeDefinition instance, string name, params Type[] parameterTypes)
+        {
+            MethodDefinition result = null;
+            for (int i = 0; i < instance.Methods.Count; i++)
+            {
+                MethodDefinition methodDefinition = instance.Methods[i];
+             
+                if (string.Equals(methodDefinition.Name, name, StringComparison.Ordinal) // Names Match
+                    && parameterTypes.Length == methodDefinition.Parameters.Count) // The same number of parameters
+                {
+                    result = methodDefinition;
+                    for (int x = methodDefinition.Parameters.Count - 1; x >= 0; x--)
+                    {
+                        ParameterDefinition parameter = methodDefinition.Parameters[x];
+                        if(!string.Equals(parameter.ParameterType.Name, parameterTypes[x].Name, StringComparison.Ordinal))
+                        {
+                            result = null;
+                            break;
+                        }
+
+                        if(x == 0 && result != null)
+                        {
+                            return result;
+                        }
+                    }
                 }
             }
             return null;
