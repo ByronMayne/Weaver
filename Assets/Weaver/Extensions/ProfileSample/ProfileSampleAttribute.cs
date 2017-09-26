f936e4a3a20b58a4fe6e50404f5483b11c66ec9c
ByronMayne <ByronMayne@gmail.com>
2017-09-25 22:54:55 -0400

    Added a comment to ProfileSampleAttribute

--- Assets/Weaver/Extensions/ProfileSample/ProfileSampleAttribute.cs
+++ Assets/Weaver/Extensions/ProfileSample/ProfileSampleAttribute.cs
@@ -2,6 +2,12 @@
 
 namespace Weaver
 {
+    /// <summary>
+    /// When put above a method and Weaver has this option
+    /// turned on this will inject <see cref="UnityEngine.Profiling.Profiler.BeginSample(string)"/>
+    /// at the start of the function and <see cref="UnityEngine.Profiling.Profiler.EndSample()"/> and the 
+    /// end. This will then output the result.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class ProfileSampleAttribute : Attribute
     {
