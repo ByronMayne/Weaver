6b6233be91d7414e3efd0039f4bfbdba4de661d0
ByronMayne <ByronMayne@gmail.com>
2017-09-25 22:54:35 -0400

    Created the OnChangedAttribute

---
+++ Assets/Weaver/Extensions/PropertyChanged/OnChangedAttribute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace Weaver
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class OnChangedAttribute : Attribute
+    {
+        public string callbackMethod { get; set; }
+
+        public OnChangedAttribute(string callbackMethod)
+        {
+            this.callbackMethod = callbackMethod;
+        }
+    }
+}
