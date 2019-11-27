ab47176ab5d191e6715738ea0cbc1f93992c8abf
ByronMayne <Byronmayne@gmail.com>
2019-11-26 21:19:36 -0500

    Added a new ILoggerInterface + type

---
+++ source/Weaver.Contracts/Diagnostics/ILogger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Weaver.Contracts.Diagnostics
+{
+    /// <summary>
+    /// Used as the logging interface for all operations that happen in Weaver. 
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Invoked to create a entry containing information
+        /// </summary>
+        /// <param name="channel">The channel is the name of the object it belongs too</param>
+        /// <param name="message">The message you want in the entry</param>
+        void Info(string channel, string message);
+
+        /// <summary>
+        /// Invoked to create a entry containing a debug log
+        /// </summary>
+        /// <param name="channel">The channel is the name of the object it belongs too</param>
+        /// <param name="message">The message you want in the entry</param>
+        void Debug(string channel, string message);
+
+        /// <summary>
+        /// Invoked to create a entry containing a warning
+        /// </summary>
+        /// <param name="channel">The channel is the name of the object it belongs too</param>
+        /// <param name="message">The message you want in the entry</param>
+        void Warning(string channel, string message);
+
+        /// <summary>
+        /// Invoked to create a entry containing an error
+        /// </summary>
+        /// <param name="channel">The channel is the name of the object it belongs too</param>
+        /// <param name="message">The message you want in the entry</param>
+        void Error(string channel, string message);
+
+        /// <summary>
+        /// Invoked to create a entry containing an exception
+        /// </summary>
+        /// <param name="channel">The channel is the name of the object it belongs too</param>
+        /// <param name="message">The message you want in the entry</param>
+        void Exception(string channel, Exception e); 
+    }
+}
---
+++ source/Weaver/Diagnostics/Logger.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using Weaver.Contracts.Diagnostics;
+
+namespace Weaver.Diagnostics
+{
+    public class Logger : ILogger
+    {
+        private Stopwatch m_sessionTimer; 
+
+        private Logger()
+        {
+            m_sessionTimer = new Stopwatch();
+        }
+
+        /// <inheritdoc >
+        public void Debug(string channel, string message)
+        {
+            Log("DBG", channel, message);
+        }
+
+        /// <inheritdoc >
+        public void Error(string channel, string message)
+        {
+            Log("ERR", channel, message);
+        }
+
+        /// <inheritdoc >
+        public void Exception(string channel, Exception e)
+        {
+            Log("EXC", channel, e.Message + "\n" + e.StackTrace);
+        }
+
+        /// <inheritdoc >
+        public void Info(string channel, string message)
+        {
+            Log("INF", channel, message);
+        }
+
+        /// <inheritdoc >
+        public void Warning(string channel, string message)
+        {
+            Log("WRN", channel, message);
+        }
+
+
+        /// <summary>Logs the specified type.</summary>
+        /// <param name="type">The type.</param>
+        /// <param name="channel">The channel.</param>
+        /// <param name="message">The message.</param>
+        private void Log(string type, string channel, string message)
+        {
+            TimeSpan time = m_sessionTimer.Elapsed;
+            Console.WriteLine($"[{time.Hours:00}:{time.Minutes:00}:{time.Seconds:00} {type}] {channel}: {message}");
+        }
+    }
+}
