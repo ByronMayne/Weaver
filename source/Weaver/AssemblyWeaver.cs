74740a8cdecfbad4bca7b0cced320ce6f46c1e7f
ByronMayne <Byronmayne@gmail.com>
2019-11-26 23:40:15 -0500

    Changed how I load assemblies

--- source/Weaver/AssemblyWeaver.cs
+++ source/Weaver/AssemblyWeaver.cs
@@ -132,6 +132,35 @@ namespace Weaver
         }
 
         /// <summary>
+        /// Weaves an assembly from disk.
+        /// </summary>
+        /// <param name="assemblyPath">The assembly path.</param>
+        /// <param name="outputLog">The output log.</param>
+        /// <param name="addIns">The add ins youu would like to run. They must from <see cref="IWeaverAddin"/> and have a zero argument constructor.</param>
+        /// <returns>
+        /// True if it's successful and false if it's not.
+        /// </returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public bool WeaveAssembly(string assemblyPath, IEnumerable<Type> addIns)
+        {
+            IList<IWeaverAddin> createdAddins = new List<IWeaverAddin>();
+
+            foreach (Type type in addIns)
+            {
+                if (!typeof(IWeaverAddin).IsAssignableFrom(type))
+                {
+                    Exception exception = new ArgumentException($"The type {type.FullName} does not inherit from the required type {typeof(IWeaverAddin)}");
+                    outputLog.Exception(nameof(AssemblyWeaver), exception);
+                }
+
+                IWeaverAddin instance = (IWeaverAddin)Activator.CreateInstance(type, true);
+                createdAddins.Add(instance);
+            }
+
+            return WeaveAssembly(assemblyPath, outputLog, addIns);
+        }
+
+        /// <summary>
         /// Gets the writer parameters for writing assemblies to disk.
         /// </summary>
         private static WriterParameters GetWriterParameters(AbsolutePath assemblyPath)
@@ -155,35 +184,6 @@ namespace Weaver
             };
 
             return writeParams;
-        }
-
-        /// <summary>
-        /// Weaves an assembly from disk.
-        /// </summary>
-        /// <param name="assemblyPath">The assembly path.</param>
-        /// <param name="outputLog">The output log.</param>
-        /// <param name="addIns">The add ins youu would like to run. They must from <see cref="IWeaverAddin"/> and have a zero argument constructor.</param>
-        /// <returns>
-        /// True if it's successful and false if it's not.
-        /// </returns>
-        /// <exception cref="NotImplementedException"></exception>
-        public bool WeaveAssembly(string assemblyPath, ILogger outputLog, IEnumerable<Type> addIns)
-        {
-            IList<IWeaverAddin> createdAddins = new List<IWeaverAddin>();
-
-            foreach (Type type in addIns)
-            {
-                if (!typeof(IWeaverAddin).IsAssignableFrom(type))
-                {
-                    Exception exception = new ArgumentException($"The type {type.FullName} does not inherit from the required type {typeof(IWeaverAddin)}");
-                    outputLog.Exception(nameof(AssemblyWeaver), exception);
-                }
-
-                IWeaverAddin instance = (IWeaverAddin)Activator.CreateInstance(type, true);
-                createdAddins.Add(instance);
-            }
-
-            return WeaveAssembly(assemblyPath, outputLog, addIns);
         }
 
         /// <summary>
@@ -259,11 +259,6 @@ namespace Weaver
             {
                 action(addin);
             }
-        }
-
-        public bool WeaveAssembly(string assemblyPath, ILogger outputLog, IEnumerable<IWeaverAddin> addIns)
-        {
-            throw new NotImplementedException();
         }
     }
 }
